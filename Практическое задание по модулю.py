99950cb8ccd40803dda22ec0b33326078e74861c
romankostuk <moryak.man@list.ru>
2024-06-07 00:33:03 +0300

    Add files via upload

---
+++ Практическое задание по модулю.py
@@ -0,0 +1,31 @@
+Aaron = [5, 3, 3, 5, 4]
+total_Aaron = sum(Aaron)
+quantity_Aaron = len(Aaron)
+average_value_Aaron = total_Aaron / quantity_Aaron
+
+Bilbo = [2, 2, 2, 3]
+total_Bilbo = sum(Bilbo)
+quantity_Bilbo = len(Bilbo)
+average_value_Bilbo = total_Bilbo / quantity_Bilbo
+
+Johnny = [4, 5, 5, 2]
+total_Johnny = sum(Johnny)
+quantity_Johnny = len(Johnny)
+average_value_Johnny = total_Johnny / quantity_Johnny
+
+Khendrik = [4, 4, 3]
+total_Khendrik = sum(Khendrik)
+quantity_Khendrik = len(Khendrik)
+average_value_Khendrik = total_Khendrik / quantity_Khendrik
+
+Steve = [5, 5, 5, 4, 5]
+total_Steve = sum(Steve)
+quantity_Steve = len(Steve)
+average_value_Steve = total_Steve / quantity_Steve
+
+sredniy_bal = {'Aaron': average_value_Aaron,
+               'Bilbo': average_value_Bilbo,
+               'Johnny': average_value_Johnny,
+               'Khendrik': average_value_Khendrik,
+               'Steve': average_value_Steve}
+print(sredniy_bal)
